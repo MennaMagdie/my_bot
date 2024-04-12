54b80bb37e7fa6e89e6bd42af2b753983f2b46ca
Menna Magdy <73966691+MennaMagdie@users.noreply.github.com>
2024-04-12 23:19:55 +0200

    Update soccer_game.launch.py

--- launch/soccer_game.launch.py
+++ launch/soccer_game.launch.py
@@ -1,40 +1,34 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
-
-from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from launch_ros.actions import Node
-
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
-        # Include the Gazebo launch file, provided by the gazebo_ros package
+        # Including the Gazebo launch file
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
              )
     
     return LaunchDescription([
-        # 1. Launch robot_state_publisher for bot1
+        # 1. Launching robot_state_publisher for bot1
         ExecuteProcess(
             cmd=['ros2', 'run', 'robot_state_publisher', 'robot_state_publisher', '~/dev_ws/src/my_bot/description/robot_urdf1.urdf', '__ns:=/bot1'],
             output='screen',
             shell=True,
         ),
-        # 2. Launch robot_state_publisher for bot2
+        # 2. Launching robot_state_publisher for bot2
         ExecuteProcess(
             cmd=['ros2', 'run', 'robot_state_publisher', 'robot_state_publisher', '~/dev_ws/src/my_bot/description/robot_urdf2.urdf', '__ns:=/bot2'],
             output='screen',
             shell=True,
         ),
-        # 3. Launch Gazebo with soccer world
+        # 3. Launching Gazebo
         gazebo,
         # 4. Spawn entity manouna for bot1
         ExecuteProcess(
@@ -48,16 +42,5 @@ def generate_launch_description():
             output='screen',
             shell=True,
         ),
-        # 6. Run teleop_twist_keyboard for bot1
-        ExecuteProcess(
-            cmd=['ros2', 'run', 'teleop_twist_keyboard', 'teleop_twist_keyboard', '--ros-args', '--remap', 'cmd_vel:=/bot1/cmd_vel'],
-            output='screen',
-            shell=True,
-        ),
-        # 7. Run teleop_twist_keyboard for bot2
-        ExecuteProcess(
-            cmd=['ros2', 'run', 'teleop_twist_keyboard', 'teleop_twist_keyboard', '--ros-args', '--remap', 'cmd_vel:=/bot2/cmd_vel'],
-            output='screen',
-            shell=True,
-        ),
+
     ])
